3d66e628100e14f67dbbdf30bc27db9f30c39e80
aryaprahladan <arya.prahladan2002@gmail.com>
2025-07-01 09:02:48 +0530

    Delete backend directory

--- backend/pitches/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import PitchListCreateView, PitchRetrieveUpdateDestroyView, InvestmentCreateView, InvestmentListView
-
-urlpatterns = [
-    path('', PitchListCreateView.as_view(), name='pitch-list-create'),
-    path('<int:pk>/', PitchRetrieveUpdateDestroyView.as_view(), name='pitch-detail'),
-    path('<int:pitch_id>/invest/', InvestmentCreateView.as_view(), name='investment-create'),
-    path('investments/', InvestmentListView.as_view(), name='investment-list'),
-]
